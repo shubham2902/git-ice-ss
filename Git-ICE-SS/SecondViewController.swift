b0feeeb46f03f11f3b77819ae6c785ef67978803
Shubham Sharma <shubham2902@gmail.com>
2016-11-14 13:23:56 -0600

    Initial Commit

---
+++ Git-ICE-SS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Git-ICE-SS
+//
+//  Created by Shubham Sharma on 11/14/16.
+//  Copyright © 2016 Shubham Sharma. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
